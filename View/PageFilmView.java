187fafae329fac9020da77efa2d65319872ca43c
BAZATORmaxime <maxime.klein03@gmail.com>
2024-04-17 15:58:17 +0200

    creation page film dans le view

---
+++ View/PageFilmView.java
@@ -0,0 +1,16 @@
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.util.List;
+
+import Modele.ListeFilms.Film;
+import Modele.ListeFilms;
+
+public class PageFilmView extends JFrame{
+
+
+}
